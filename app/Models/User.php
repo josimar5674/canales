0edbb84874d8aa9ce6818d89f1550748b103450f
Danilo Herrera Elvir <daniloherreraelvir@MacBook-Pro-de-Danilo.local>
2026-05-16 09:47:24 -0600

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable([
+
+class User extends Authenticatable
+{
+
+    protected $fillable = [
     'name',
     'email',
     'password',
@@ -18,10 +20,12 @@ use Illuminate\Notifications\Notifiable;
     'photo',
     'role',
     'active'
-])]
-#[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
-{
+];
+
+protected $hidden = [
+    'password',
+    'remember_token',
+];
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
